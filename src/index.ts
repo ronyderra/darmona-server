c7e9d14a4f576f2232ff3ec8ffdd4fb2124aaa5a
ronyderra <derrarony@gmail.com>
2023-07-17 14:42:23 +0300

    port

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { config } from "dotenv";
 import { Server } from "socket.io";
 import http from "http";
+import router from "./routes/routes";
 config();
 
 const port = process.env.PORT || 3030;
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+app.use("/", router);
 
 export const server = http.createServer(app);
 
