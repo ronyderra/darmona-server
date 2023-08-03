c0c55cb18c759402ae3668dd0dece7c26113e69c
ronyderra <derrarony@gmail.com>
2023-08-03 14:26:40 +0300

    router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,20 @@ import addUser from "../controller/addUser";
 
 const router = express.Router();
 const validate = new UserController();
-router.get("/addUser", validate.checkRequestBody , addUser);
+
+router.post("/addUser", validate.checkRequestBody , addUser);
+router.post("/deleteUser", validate.checkRequestBody , addUser);
+router.put("/updateUser", validate.checkRequestBody , addUser);
+router.get("/getUser", validate.checkRequestBody , addUser);
+
+// router.post("/addBlackPage", validate.checkRequestBody , addBlackPage);
+// router.post("/deleteBlackPage", validate.checkRequestBody , addBlackPage);
+// router.put("/updateBlackPage", validate.checkRequestBody , addBlackPage);
+// router.get("/getBlackPage", validate.checkRequestBody , addBlackPage);
+
+// router.post("/addWhitePage", validate.checkRequestBody , addWhitePage);
+// router.post("/deleteWhitePage", validate.checkRequestBody , addWhitePage);
+// router.put("/updateWhitePage", validate.checkRequestBody , addWhitePage);
+// router.get("/getWhitePage", validate.checkRequestBody , addWhitePage);
 
 export default router;
