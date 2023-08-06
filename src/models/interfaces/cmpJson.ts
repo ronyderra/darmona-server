25dc5da91fc305649eab810f0dc990718c6bc7aa
ronyderra <derrarony@gmail.com>
2023-08-06 14:59:43 +0300

    Add_CMP_JSON

---
+++ src/models/interfaces/cmpJson.ts
@@ -0,0 +1,42 @@
+export interface Add_CMP_JSON {
+  imp: IMP;
+  ctype: string;
+  query: any;
+  alias: string;
+  ip: boolean;
+  track: boolean;
+  dc_ep: string;
+  query_map: any;
+  eps: EPS[];
+}
+
+export interface FULL_CMP_JSON {
+  id: string;
+  hid: string;
+  imp: IMP;
+  ctype: string;
+  query: any;
+  alias: string;
+  ip: boolean;
+  track: boolean;
+  dc_ep: string;
+  query_map: any;
+  eps: EPS[];
+}
+
+export interface IMP {
+  count: number;
+  tsp?: number;
+  recurring: boolean;
+}
+
+export interface EPS {
+  geo: EPS_GEO;
+  weight: number;
+  ep: string;
+}
+
+export interface EPS_GEO {
+  grp: string[];
+  bl: boolean;
+}
