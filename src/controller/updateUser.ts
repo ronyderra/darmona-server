2543d1a16a08af6ac0e967ded1403afc8ebc1a95
ronyderra <derrarony@gmail.com>
2023-08-06 14:23:39 +0300

    updateUser

--- src/controller/updateUser.ts
+++ src/controller/updateUser.ts
@@ -1,44 +1,47 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import USER from "../models/user";
-import { IUSER  ,IUSERDocument} from "../models/interfaces/user";
+import { IUSER, IUSERDocument } from "../models/interfaces/user";
 
 const updateUser = async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  // const userToUpdate: IUSERDocument | undefined = USER.getById(req.body._id);
+  const userToUpdate: IUSERDocument | undefined = await USER.getById(
+    req.body._id
+  );
 
-  // switch (true) {
-  //   case req.body.username:
-  //     userToUpdate.username = req.body.username;
-  //     break;
-  //   case req.body.password:
-  //     userToUpdate.username = req.body.password;
-  //     break;
-  //   case req.body.aliases:
-  //     userToUpdate.username = req.body.aliases;
-  //     break;
-  //   case req.body.thriveId:
-  //     userToUpdate.username = req.body.thriveId;
-  //     break;
-  //   case req.body.email:
-  //     userToUpdate.username = req.body.email;
-  //     break;
-  //   case req.body.blackPageDomains:
-  //     userToUpdate.username = req.body.blackPageDomains;
-  //     break;
-  //   case req.body.cmps:
-  //     userToUpdate.username = req.body.cmps;
-  //     break;
-  //   case req.body.role:
-  //     userToUpdate.username = req.body.role;
-  //     break;
+  //validate that there is only 1 alias/blackPageDomains/cmps to push else loop it
 
-  //   default:
-  //     break;
-  // }
+  switch (true) {
+    case req.body.username:
+      userToUpdate.username = req.body.username;
+
+    case req.body.password:
+      userToUpdate.password = req.body.password;
+
+    case req.body.aliases:
+      userToUpdate.aliases.push(req.body.aliases);
+
+    case req.body.thriveId:
+      userToUpdate.thriveId = req.body.thriveId;
+
+    case req.body.email:
+      userToUpdate.email = req.body.email;
+
+    case req.body.blackPageDomains:
+      userToUpdate.blackPageDomains.push(req.body.blackPageDomains);
+
+    case req.body.cmps:
+      userToUpdate.cmps.push(req.body.cmps);
+
+    case req.body.role:
+      userToUpdate.role = req.body.role;
+
+    default:
+      break;
+  }
 
   //   const result = await USER.findUser(String(username), String(password));
   //   return res.status(200).json(result);
