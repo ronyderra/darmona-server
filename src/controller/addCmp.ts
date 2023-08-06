25f07fc5862aa21c4dbb89ebc4d4e17c24807e41
ronyderra <derrarony@gmail.com>
2023-08-06 17:43:05 +0300

    addCmp

--- src/controller/addCmp.ts
+++ src/controller/addCmp.ts
@@ -14,6 +14,7 @@ const addCmp = async (req: Request, res: Response) => {
   }
 
   const user = await USER.getById(new ObjectId(String(req.body._id)));
+  console.log(user);
   if (!user) {
     return res.status(400).send("user not found");
   }
@@ -24,27 +25,27 @@ const addCmp = async (req: Request, res: Response) => {
     const json: FULL_CMP_JSON = {
       id: Number(id),
       hid,
-      imp: JSON.parse(req.body.imp),
+      imp: req.body.imp,
       ctype: req.body.ctype,
-      query: JSON.parse(req.body.query),
+      query: req.body.query,
       alias: req.body.alias,
       ip: req.body.ip,
       track: req.body.track,
       dc_ep: req.body.dc_ep,
-      query_map: JSON.parse(req.body.query_map),
-      eps: JSON.parse(req.body.eps),
+      query_map: req.body.query_map,
+      eps: req.body.eps,
     };
-    console.log({ json });
-    const file = await s3FileManager.createFile("testing", json);
-    if (file) {
-      await incrementIdAndSave();
-      user.cmps.push({
-        name: req.body.name,
-        url: `https://${req.body.alias}/?cmp=${hid}`,
-      });
-      const updated = await USER.updateById(req.body._id, user);
-      return res.status(200).json(updated);
-    }
+    console.log( json );
+    // const file = await s3FileManager.createFile("testing", json);
+    // if (file) {
+    //   await incrementIdAndSave();
+    //   user.cmps.push({
+    //     name: req.body.name,
+    //     url: `https://${req.body.alias}/?cmp=${hid}`,
+    //   });
+    //   const updated = await USER.updateById(req.body._id, user);
+    //   return res.status(200).json(updated);
+    // }
   } catch (error) {
     console.log(error.message);
     return res.status(404).json(error.message);
