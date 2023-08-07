7a3e0057052bbe6f5d873e2e049443a5ecdcba1a
ronyderra <derrarony@gmail.com>
2023-08-07 16:08:50 +0300

    updateUser

--- src/controller/updateUser.ts
+++ src/controller/updateUser.ts
@@ -13,29 +13,29 @@ const updateUser = async (req: Request, res: Response) => {
   );
 
   //validate that there is only 1 alias/blackPageDomains/cmps to push else loop it
-  if (req.body.username) {
-    userToUpdate.username = req.body.username;
+  if (req.body.data.username) {
+    userToUpdate.username = req.body.data.username;
   }
-  if (req.body.password) {
-    userToUpdate.password = req.body.password;
+  if (req.body.data.password) {
+    userToUpdate.password = req.body.data.password;
   }
-  if (req.body.aliases) {
-    userToUpdate.aliases.push(req.body.aliases);
+  if (req.body.data.alias) {
+    userToUpdate.aliases.push(req.body.data.alias);
   }
-  if (req.body.thriveId) {
-    userToUpdate.thriveId = req.body.thriveId;
+  if (req.body.data.thriveId) {
+    userToUpdate.thriveId = req.body.data.thriveId;
   }
-  if (req.body.email) {
-    userToUpdate.email = req.body.email;
+  if (req.body.data.email) {
+    userToUpdate.email = req.body.data.email;
   }
-  if (req.body.blackPageDomains) {
-    userToUpdate.blackPageDomains.push(req.body.blackPageDomains);
+  if (req.body.data.blackPageDomain) {
+    userToUpdate.blackPageDomains.push(req.body.data.blackPageDomain);
   }
-  if (req.body.cmps) {
-    userToUpdate.cmps.push(req.body.cmps);
+  if (req.body.data.cmp) {
+    userToUpdate.cmps.push(req.body.data.cmp);
   }
-  if (req.body.role) {
-    userToUpdate.role = req.body.role;
+  if (req.body.data.role) {
+    userToUpdate.role = req.body.data.role;
   }
   
   const result = await USER.updateById(req.body._id, userToUpdate);
