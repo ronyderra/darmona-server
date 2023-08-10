bc58e1ee0dad99deea30859bd348fa08fd344c55
ronyderra <derrarony@gmail.com>
2023-08-10 10:52:26 +0300

    app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-import { Server } from "socket.io";
 import http from "http";
 import router from "./routes/routes";
 import mongoose from "mongoose";
@@ -13,23 +12,19 @@ const MongoUrl: string = process.env.MONGO_URL || "";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000" ,"https://master.d7vuyvgacqzf1.amplifyapp.com" ] }));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "https://master.d7vuyvgacqzf1.amplifyapp.com",
+    ],
+  })
+);
 app.use("/", router);
 
-export const server = http.createServer(app);
-export const clientAppSocket = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-clientAppSocket.setMaxListeners(0);
-clientAppSocket.on("connection", (socket) => {
-  // console.log("a user connected to ", socket.id);
-  // clientAppSocket.emit("updateAchievment", socket.id)
-});
-
-export default server.listen(port, async () => {
+const server = http.createServer(app);
+server.listen(port, async () => {
   console.log(`Server runs on port ${port}`);
 });
 
