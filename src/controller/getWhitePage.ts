9fded35faf5fac7a218b9544c38ca97ff8d7a64c
ronyderra <derrarony@gmail.com>
2023-08-13 19:18:39 +0300

    getWhitePage

--- src/controller/getWhitePage.ts
+++ src/controller/getWhitePage.ts
@@ -2,18 +2,22 @@ import { Request, Response } from "express";
 import WHITE_PAGES from "../models/whitePage";
 
 const getWhitePage = async (req: Request, res: Response) => {
-  const { language, topic, domain } = req.query;
-  if (language==="" && topic==="" && domain !== "") {
-    const result = await WHITE_PAGES.getLangsByDomain(String(domain));
-    return res.status(200).json(result);
-  } else if (language && topic && domain) {
-    const result = await WHITE_PAGES.findWhitePage(
-      String(language),
-      String(topic),
-      String(domain)
-    );
-    return res.status(200).json(result);
-  } else {
+  try {
+    const { language, topic, domain } = req.query;
+    if (language === "" && topic === "" && domain !== "") {
+      const result = await WHITE_PAGES.getLangsByDomain(String(domain));
+      return res.status(200).json(result);
+    } else if (language && topic && domain) {
+      const result = await WHITE_PAGES.findWhitePage(
+        String(language),
+        String(topic),
+        String(domain)
+      );
+      return res.status(200).json(result);
+    } else {
+      return res.status(400).send("you must send language && topic && domain");
+    }
+  } catch (error) {
     return res.status(400).send("you must send language && topic && domain");
   }
 };
