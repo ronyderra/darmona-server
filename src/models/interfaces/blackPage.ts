dd1096a884f9eac1843509519c342a406ec8bbcb
ronyderra <derrarony@gmail.com>
2023-08-17 14:07:20 +0300

    IBLACKPAGE

--- src/models/interfaces/blackPage.ts
+++ src/models/interfaces/blackPage.ts
@@ -2,11 +2,13 @@ import { Document, Model } from "mongoose";
 
 export interface IBLACKPAGE {
   domain: string;
-  lang:string;
-  template:string;
-  celeb:string;
-  offerPage:string;
-  url:string;
+  lang: string;
+  geo: string;
+  template: string;
+  celeb: string;
+  offerPage: string;
+
+  url: string;
 }
 
 export interface IBLACKPAGDocument extends IBLACKPAGE, Document {
