ffb31fd6469482bd1ebf4150db6e138ef97a3094
ronyderra <derrarony@gmail.com>
2023-08-24 22:03:17 +0300

    getBlackPages

--- src/controller/getBlackPages.ts
+++ src/controller/getBlackPages.ts
@@ -10,12 +10,21 @@ export const getBlackPages = async (req: Request, res: Response) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  const { path } = req.query;
+  const { path, bucketName } = req.query;
+  let resp;
+  
+  if (bucketName !== "") {
+    console.log("got herte");
+    resp = await s3plfm.getAllFiles(path, String(bucketName));
+   
+    
+  } else {
+    resp = await s3plfm.getAllFiles(path);
+  }
 
-  const resp = await s3plfm.getAllFiles(path);
+
   const data = resp?.CommonPrefixes?.map((i) => i.Prefix);
   if (data) {
-    // console.log(data);
     return res.status(200).json({ data });
   } else {
     return res.status(400).json({ errors: "failed" });
@@ -28,17 +37,14 @@ export const getCharacters = async (req: Request, res: Response) => {
     return res.status(400).json({ errors: errors.array() });
   }
   const { path } = req.query;
-
-  const resp = await s3plfm.getAllFiles(path , process.env.ASSETS_BUCKET);
-console.log(resp);
-
-  const data = resp?.Contents.length > 0 ? resp?.Contents?.map((i) => i.Key) : resp.CommonPrefixes.map((i) => i.Prefix);;
+  const resp = await s3plfm.getAllFiles(path, process.env.ASSETS_BUCKET);
+  const data =
+    resp?.Contents.length > 0
+      ? resp?.Contents?.map((i) => i.Key)
+      : resp.CommonPrefixes.map((i) => i.Prefix);
   if (data) {
-    console.log(data);
     return res.status(200).json({ data });
   } else {
     return res.status(400).json({ errors: "failed" });
   }
 };
-
-
