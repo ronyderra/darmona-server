e05a846c295ffd5cc08660d72d4e4cdf9b052de4
ronyderra <derrarony@gmail.com>
2023-09-12 14:45:55 +0300

    router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { UserController } from "./validation";
 import addUser from "../controller/addUser";
 import getUser from "../controller/getUser";
-import {getWhitePage, getWhitePageHtmlType} from "../controller/getWhitePage";
+import { getWhitePage, getWhitePageHtmlType } from "../controller/getWhitePage";
 import { getBlackPages, getCharacters } from "../controller/getBlackPages";
 import login from "../controller/login";
 import addCmp from "../controller/addCmp";
@@ -10,18 +10,10 @@ import getAvailableAliases from "../controller/getAvailableAliases";
 import updateUser from "../controller/updateUser";
 import updateCmp from "../controller/updateCmp";
 import getCmp from "../controller/getCmp";
-import {
-  getSnowData,
-  getRows,
-  countByDateAndParam,
-} from "../controller/getSnowData";
+import { getSnowData, getRows, countByDateAndParam } from "../controller/getSnowData";
 import { config } from "dotenv";
 import jwt from "jsonwebtoken";
-import {
-  getBlackPagesV2,
-  getGeos,
-  getCharactersV2,
-} from "../controller/getBlackPagesV2";
+import { getBlackPagesV2, getGeos, getCharactersV2 } from "../controller/getBlackPagesV2";
 import { uploadImg } from "../controller/uploadImg";
 import { bycmpId } from "../controller/cmps/bycmpId";
 config();
@@ -30,16 +22,12 @@ function validateBearerToken(req, res, next) {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Unauthorized - Bearer token not found" });
+    return res.status(401).json({ error: "Unauthorized - Bearer token not found" });
   }
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.BEARER, (err, decodedToken) => {
     if (err) {
-      return res
-        .status(401)
-        .json({ error: "Unauthorized - Invalid bearer token" });
+      return res.status(401).json({ error: "Unauthorized - Invalid bearer token" });
     }
     req.decodedToken = decodedToken;
     next();
@@ -52,57 +40,24 @@ const validate = new UserController();
 router.post("/login", validate.login(), login);
 router.get("/getUser", validateBearerToken, validate.getUser(), getUser);
 router.post("/addUser", validateBearerToken, validate.addUser(), addUser);
-router.put(
-  "/updateUser",
-  validateBearerToken,
-  validate.updateUser(),
-  updateUser
-);
+router.put("/updateUser", validateBearerToken, validate.updateUser(), updateUser);
 
 router.post("/addCmp", validateBearerToken, validate.addCmp(), addCmp);
 router.put("/updateCmp", validateBearerToken, validate.updateCmp(), updateCmp);
 router.get("/getCmp", validateBearerToken, validate.getCmp(), getCmp);
 
-router.get(
-  "/getAvailableAliases",
-  validateBearerToken,
-  validate.getAvailableAliases(),
-  getAvailableAliases
-);
+router.get("/getAvailableAliases", validateBearerToken, validate.getAvailableAliases(), getAvailableAliases);
 
-router.get(
-  "/snowData",
-  validateBearerToken,
-  validate.getSnowData(),
-  getSnowData
-);
-router.get(
-  "/countByDateAndParam",
-  validateBearerToken,
-  validate.countByDateAndParam(),
-  countByDateAndParam
-);
+router.get("/snowData", validateBearerToken, validate.getSnowData(), getSnowData);
+router.get("/countByDateAndParam", validateBearerToken, validate.countByDateAndParam(), countByDateAndParam);
 router.get("/getrows", validateBearerToken, validate.getSnowData(), getRows);
 
 router.get("/whitePage", validateBearerToken, getWhitePage);
 router.get("/getWhitePageHtmlType", validateBearerToken, getWhitePageHtmlType);
 
-// router.post("/addBlackPage", validate.checkRequestBody , addBlackPage);
-// router.post("/deleteBlackPage", validate.checkRequestBody , addBlackPage);
-// router.put("/updateBlackPage", validate.checkRequestBody , addBlackPage);
-router.get(
-  "/getBlackPages",
-  validateBearerToken,
-  validate.getBlackPages(),
-  getBlackPages
-);
+router.get("/getBlackPages", validateBearerToken, validate.getBlackPages(), getBlackPages);
 router.post("/uploadImg", validateBearerToken, uploadImg);
-router.get(
-  "/getCharacters",
-  validateBearerToken,
-  validate.getBlackPages(),
-  getCharacters
-);
+router.get("/getCharacters", validateBearerToken, validate.getBlackPages(), getCharacters);
 
 router.get("/getBlackPagesV2", getBlackPagesV2);
 router.get("/getGeos", getGeos);
