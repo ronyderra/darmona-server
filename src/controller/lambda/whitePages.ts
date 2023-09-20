3f61d9298119c174d41139cfdb984c1112cb0699
ronyderra <derrarony@gmail.com>
2023-09-20 15:38:09 +0300

    handleNewWhitePage

--- src/controller/lambda/whitePages.ts
+++ src/controller/lambda/whitePages.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
-import WHITE_PAGES from "../../models/whitePage";
+import WHITE_PAGES, { IWHITEPAGE } from "../../models/whitePage";
 
 export const handleNewWhitePage = async (req: Request, res: Response) => {
   try {
     const { key } = req.query;
-    console.log(key);
-    // const result = await WHITE_PAGES.getWhitePageHtmlType();
-    return res.status(200).send(key);
+    console.log("New white page in templatesd: ", { key });
+    res.status(200).send(key);
+
+    const keySplit = String(key).split("/");
+
+    const doc: IWHITEPAGE = {
+      language: keySplit[1],
+      topic: keySplit[2],
+      link: String(key).replace("templates/", ""),
+      domain: "any",
+      linkType: "html",
+    };
+    const result = await WHITE_PAGES.createNew(doc);
+    console.log({ result });
+    return;
   } catch (error) {
     return res.status(400).send("you must send language && topic && domain");
   }
