b1edc38bfcad4bb8b758b4c6725eeb2c1fb1406a
ronyderra <derrarony@gmail.com>
2023-09-20 15:11:50 +0300

    handleNewWhitePage

--- src/controller/lambda/whitePages.ts
+++ src/controller/lambda/whitePages.ts
@@ -4,10 +4,9 @@ import WHITE_PAGES from "../../models/whitePage";
 export const handleNewWhitePage = async (req: Request, res: Response) => {
   try {
     const { key } = req.query;
-    const result = await WHITE_PAGES.getWhitePageHtmlType();
-    console.log(result);
-
-    return res.status(200).json(result);
+    console.log(key);
+    // const result = await WHITE_PAGES.getWhitePageHtmlType();
+    return res.status(200).send(key);
   } catch (error) {
     return res.status(400).send("you must send language && topic && domain");
   }
